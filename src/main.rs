896453163c547e0166a5906b9586a1d1358d957a
Astrid Yu <astrid@astrid.tech>
2026-04-17 15:05:24 -0700

    trippy core repro

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::net::IpAddr;
+use std::str::FromStr;
+use trippy_core::Builder;
+
+fn main() {
+    let _fds = make_a_bunch_of_fds();
+
+    // copied nearly verbatim from docs.rs example
+    let addr = IpAddr::from_str("1.1.1.1").unwrap();
+    Builder::new(addr)
+        .build()
+        .unwrap()
+        .run_with(|round| println!("{:?}", round))
+        .unwrap();
+}
+
+fn make_a_bunch_of_fds() -> Vec<std::fs::File> {
+    (0..1024)
+        .map(|_| std::fs::File::open("/dev/null").unwrap())
+        .collect()
+}
