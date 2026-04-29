ab4a81b27c8446403dc8aac92fc636b636cb53fc
Astrid Yu <astrid@astrid.tech>
2026-04-29 13:56:31 -0700

    apply changes from https://github.com/fujiapple852/trippy/pull/1796

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::net::IpAddr;
 use std::str::FromStr;
 use trippy_core::Builder;
+use trippy_core::SocketReadinessMode;
 
 fn main() {
     let _fds = make_a_bunch_of_fds();
@@ -8,6 +9,7 @@ fn main() {
     // copied nearly verbatim from docs.rs example
     let addr = IpAddr::from_str("1.1.1.1").unwrap();
     Builder::new(addr)
+        .socket_readiness_mode(SocketReadinessMode::Poll)
         .build()
         .unwrap()
         .run_with(|round| println!("{:?}", round))
